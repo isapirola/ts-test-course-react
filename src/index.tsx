a268515a04aff6b6caf0413942b111326d06b75e
IsaPirola <belapirola@gmail.com>
2024-02-27 15:56:20 -0300

    import components for reac testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from "react-dom/client";
-import App2 from "./App2";
+import LoginComponent from "./LoginComponent";
+import LoginService from "./services/LoginService";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(<App2 />);
+const loginService = new LoginService();
+
+const setToken = (token: string) => {
+    console.log(`received the token ${token}`);
+};
+
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />);

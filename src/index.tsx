370ab2504ee78f1dc5366952789116e73209f260
IsaPirola <belapirola@gmail.com>
2024-02-27 15:46:24 -0300

    understanding some basics of react

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App2 from "./App2";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+
+root.render(<App2 />);
